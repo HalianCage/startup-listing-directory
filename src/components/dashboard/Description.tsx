9f0ceeb46548164eea3dcccf0b18fd9aab1f3707
HalianCage <akartikshinded@gmail.com>
2026-02-06 14:55:27 +0530

    Added logo on home page Added the startup India logo on the home page. Also deleted some unsued code files.

--- src/components/dashboard/Description.tsx
+++ src/components/dashboard/Description.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
+
 export const Description = () => {
     return (
         <div className="flex flex-col items-center justify-center rounded-2xl border border-gray-200 bg-white px-5 pb-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6">
-            <h1 className="text-4xl font-bold text-orange-350">
-            Startup India - India's Largest Startup Funding Directory
+            <Image height={500} width={700} src={"/images/logo/startup-india.svg"} alt="Startup India"/>
+            <h1 className="mt-10 text-4xl font-bold text-orange-350">
+            The India's Largest Startup Funding Directory
             </h1>
             <p className="text-xl mt-5 text-gray-500 text-theme-sm dark:text-gray-400">
             Browse through Indian startups funded in recent years.
